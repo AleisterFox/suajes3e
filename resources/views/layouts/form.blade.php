d5f50933425f21e01fb827c5d852b4f3a02b8c1d
AleisterFox <ericaguirrez@hotmail.com>
2023-02-05 23:06:26 -0600

    Delete register option in login view

--- resources/views/layouts/form.blade.php
+++ resources/views/layouts/form.blade.php
@@ -50,12 +50,7 @@
           <!-- Navbar items -->
           <ul class="navbar-nav ml-auto">
             
-            <li class="nav-item">
-              <a class="nav-link nav-link-icon" href="{{route('register')}}">
-                <i class="ni ni-circle-08"></i>
-                <span class="nav-link-inner--text">Register</span>
-              </a>
-            </li>
+            
             <li class="nav-item">
               <a class="nav-link nav-link-icon" href="{{route ('login')}}">
                 <i class="ni ni-key-25"></i>
