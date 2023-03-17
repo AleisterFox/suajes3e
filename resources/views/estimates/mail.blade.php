5c1b9129e8a28b01c6a006de9736dd37161b443b
AleisterFox <ericaguirrez@hotmail.com>
2023-03-17 13:28:50 -0600

    change href link in estimate mail template

--- resources/views/estimates/mail.blade.php
+++ resources/views/estimates/mail.blade.php
@@ -12,6 +12,6 @@
 
     <p>Adjuntamos la cotizacion del servicio solicitado</p>
     <p>Dando click en el siguiente boton</p>
-    <a style="text-decoration-line: none; width: 60px; height:30px; color: #fff; padding: 0.375rem 0.75rem; vertical-align: middle; text-align: center; font-weight: 600; border-radius: 0.25rem; border: 1px solid transparent; background-color: #08a7e5; border-color: #08a7e5; box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);" href="eric-agrez.com/customer/{{$cliente}}/estimates/{{$cotizacion}}" target="_blank" rel="noopener noreferrer">Abrir</a>   
+    <a style="text-decoration-line: none; width: 60px; height:30px; color: #fff; padding: 0.375rem 0.75rem; vertical-align: middle; text-align: center; font-weight: 600; border-radius: 0.25rem; border: 1px solid transparent; background-color: #08a7e5; border-color: #08a7e5; box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);" href="suajes3e-elias.com/customer/{{$cliente}}/estimates/{{$cotizacion}}" target="_blank" rel="noopener noreferrer">Abrir</a>   
 </body>
 </html>
